71330ffec45ae8bb93ad267d31be2f8293277b58
MemesPwr <47759467+MemesPwr@users.noreply.github.com>
2024-06-30 16:38:22 +0300

    export by original (en)

--- export.py
+++ export.py
@@ -21,18 +21,18 @@ with open(csv_out, 'w', encoding='utf-8', newline='') as csv_file_content:
                 sql_cur = sql_con.cursor()
                 sql_cur_orig = sql_con_orig.cursor()
 
-                sql_cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-                tables = sql_cur.fetchall()
+                sql_cur_orig.execute("SELECT name FROM sqlite_master WHERE type='table';")
+                tables = sql_cur_orig.fetchall()
                 for table_name_ex in tables:
                     table_name = table_name_ex[0]
                     print(f'Processing table "{table_name}"...')
-                    sql_cur.execute(f"SELECT Id,Content FROM '{table_name}';")
-                    data = sql_cur.fetchall()
+                    sql_cur_orig.execute(f"SELECT Id,Content FROM '{table_name}';")
+                    data = sql_cur_orig.fetchall()
                     for r_id, content in data:
-                        sql_cur_orig.execute(f"SELECT Content FROM '{table_name}' where Id = '{r_id}';")
-                        orig_values = sql_cur_orig.fetchall()
-                        orig_value = ""
-                        if len(orig_values) > 0 and len(orig_values[0]) > 0:
-                            orig_value = orig_values[0][0]
+                        sql_cur.execute(f"SELECT Content FROM '{table_name}' where Id = '{r_id}';")
+                        ru_values = sql_cur.fetchall()
+                        ru_value = ""
+                        if len(ru_values) > 0 and len(ru_values[0]) > 0:
+                            ru_value = ru_values[0][0]
 
-                        csv_file.writerow((file, table_name, r_id, orig_value, content))
+                        csv_file.writerow((file, table_name, r_id, content, ru_value))
